3e13bc686b63718cff827d9aed00b147bd9ecd45
javadrip <javadripdev@gmail.com>
2023-08-21 22:59:33 +0800

    Draw a random walk

--- angela-yu-100-days-of-code/turtle/main.py
+++ angela-yu-100-days-of-code/turtle/main.py
@@ -10,12 +10,14 @@ t = Turtle()
 #     t.forward(100)
 #     t.right(90)
 
+
 # Draw dotted line
 # for _ in range(20):
 #     t.forward(5)
 #     t.penup()
 #     t.forward(5)
 #     t.pendown()
+
 
 # Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon overlaid over each other
 colours = [
@@ -32,12 +34,24 @@ colours = [
     "gray",
 ]
 
-for i in range(3, 11):
+# for i in range(3, 11):
+#     t.color(random.choice(colours))
+#     for j in range(i):
+#         t.forward(100)
+#         angle = 360 / i
+#         t.right(angle)
+
+
+# Draw a random walk
+t.hideturtle()
+t.width(10)
+t.speed(0)
+angle = [0, 90, 180, 270]
+
+for _ in range(200):
     t.color(random.choice(colours))
-    for j in range(i):
-        t.forward(100)
-        angle = 360 / i
-        t.right(angle)
+    t.forward(20)
+    t.setheading(random.choice(angle))
 
 
 screen = Screen()
