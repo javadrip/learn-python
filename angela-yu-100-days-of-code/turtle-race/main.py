cdb083d0ac9c942323b3fd139f8a48dfaaf85ca8
javadrip <javadripdev@gmail.com>
2023-08-22 11:49:27 +0800

    Create instances of turtles

--- angela-yu-100-days-of-code/turtle-race/main.py
+++ angela-yu-100-days-of-code/turtle-race/main.py
@@ -1,9 +1,22 @@
 import turtle as t
 
-tim = t.Turtle()
+# tim = t.Turtle()
 screen = t.Screen()
 screen.setup(width=500, height=400)
+user_bet = screen.textinput(
+    title="Make your bet", prompt="Which turtle will win the race? Enter a color: "
+)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
+y = -140
+
+for color in colors:
+    gap = 40
+    y += gap
+    tim = t.Turtle(shape="turtle")
+    tim.color(color)
+    tim.penup()
+    tim.goto(x=-230, y=y)
 
 
 screen.exitonclick()
