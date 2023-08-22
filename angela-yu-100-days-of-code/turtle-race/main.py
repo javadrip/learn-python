2f5c39b64ace8d76414f3808af8e3d2c0c6cb34e
javadrip <javadripdev@gmail.com>
2023-08-22 12:03:09 +0800

    Complete turtle race

--- angela-yu-100-days-of-code/turtle-race/main.py
+++ angela-yu-100-days-of-code/turtle-race/main.py
@@ -1,6 +1,8 @@
+import random
 import turtle as t
 
 # tim = t.Turtle()
+is_race_on = False
 screen = t.Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(
@@ -10,6 +12,8 @@ colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 y = -140
 
+all_turtles = []
+
 for color in colors:
     gap = 40
     y += gap
@@ -17,6 +21,23 @@ for color in colors:
     tim.color(color)
     tim.penup()
     tim.goto(x=-230, y=y)
+    all_turtles.append(tim)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() >= 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost... The {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 
 screen.exitonclick()
