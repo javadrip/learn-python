a59069a62fe66d6ac3fbdb40b93dba95d9e2ae78
javadrip <javadripdev@gmail.com>
2023-08-22 10:50:31 +0800

    Complete Hirst spots

--- angela-yu-100-days-of-code/hirst-spots/main.py
+++ angela-yu-100-days-of-code/hirst-spots/main.py
@@ -18,6 +18,11 @@
 
 # print(rgb_colors)
 
+import random
+import turtle as t
+
+t.colormode(255)
+
 color_list = [
     (204, 165, 107),
     (239, 246, 241),
@@ -28,3 +33,29 @@ color_list = [
     (51, 93, 125),
     (137, 31, 21),
 ]
+
+tim = t.Turtle()
+
+tim.penup()
+tim.speed(0)
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+number_of_dots = 100
+
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = t.Screen()
+screen.exitonclick()
