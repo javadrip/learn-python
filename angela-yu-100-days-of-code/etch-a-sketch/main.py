fb936e1fd1ea28c47432d6104a67d2fd02dab611
javadrip <javadripdev@gmail.com>
2023-08-22 11:08:28 +0800

    Etch-a-sketch exercise begin

---
+++ angela-yu-100-days-of-code/etch-a-sketch/main.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+tim = t.Turtle()
+screen = t.Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+screen.listen()
+screen.onkey(key="space", fun=move_forward)
+screen.exitonclick()
