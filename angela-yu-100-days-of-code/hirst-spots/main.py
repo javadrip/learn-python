81dc38aef72a8313c12834f58adee4d0f848102d
javadrip <javadripdev@gmail.com>
2023-08-22 10:50:16 +0800

    Extracted colors

---
+++ angela-yu-100-days-of-code/hirst-spots/main.py
@@ -0,0 +1,30 @@
+# import colorgram
+
+# number_of_colors = 10
+
+# colors = colorgram.extract("image.png", number_of_colors)
+
+# rgb_colors = []
+
+# for color in colors:
+#     combination = ()
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+
+#     new_color = (r, g, b)
+
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+
+color_list = [
+    (204, 165, 107),
+    (239, 246, 241),
+    (151, 73, 47),
+    (235, 238, 243),
+    (223, 202, 135),
+    (172, 154, 38),
+    (51, 93, 125),
+    (137, 31, 21),
+]
