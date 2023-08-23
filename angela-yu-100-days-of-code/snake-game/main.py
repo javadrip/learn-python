f227c587be6636b78d1c090b1e8bd3e64a8ab383
Vinleon Ang <javadripdev@gmail.com>
2023-08-23 11:10:25 +0900

    Create individual segments

--- angela-yu-100-days-of-code/snake-game/main.py
+++ angela-yu-100-days-of-code/snake-game/main.py
@@ -4,3 +4,18 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+
+# Create 3-unit square length snake
+segment_1 = Turtle("square")
+segment_1.color("white")
+
+segment_2 = Turtle("square")
+segment_2.color("white")
+segment_2.goto(-20, 0)
+
+
+segment_3 = Turtle("square")
+segment_3.color("white")
+segment_3.goto(-40, 0)
+
+screen.exitonclick()
