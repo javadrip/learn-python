72d70aebf3e6b57c25ddff05cfa95022bbd87454
Vinleon Ang <javadripdev@gmail.com>
2023-08-24 15:52:45 +0900

    Get snake to move as a whole

--- angela-yu-100-days-of-code/snake-game/main.py
+++ angela-yu-100-days-of-code/snake-game/main.py
@@ -1,9 +1,12 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+# Need to be disabled before screen.update() can work
+screen.tracer(0)
 
 # Create 3-unit square length snake
 # segment_1 = Turtle("square")
@@ -21,10 +24,23 @@ screen.title("Snake Game")
 # Using a for loop to create segments
 starting_positions = [(0,0), (-20, 0), (-40,0)]
 
+segments = []
+
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
 
+game_is_on = True
+
+while game_is_on:
+    # Updates the screen only after each segment is looped through
+    # i.e. snake appears to move as one whole instead of segment by segment
+    screen.update()
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
 
 screen.exitonclick()
