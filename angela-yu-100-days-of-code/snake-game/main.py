29462651c987b33543f0453a1287b2ad87e0b1c4
Vinleon Ang <javadripdev@gmail.com>
2023-08-24 15:11:42 +0900

    Create segments using while loop

--- angela-yu-100-days-of-code/snake-game/main.py
+++ angela-yu-100-days-of-code/snake-game/main.py
@@ -6,16 +6,25 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 # Create 3-unit square length snake
-segment_1 = Turtle("square")
-segment_1.color("white")
+# segment_1 = Turtle("square")
+# segment_1.color("white")
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20, 0)
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
 
 
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.goto(-40, 0)
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
+
+# Using a for loop to create segments
+starting_positions = [(0,0), (-20, 0), (-40,0)]
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
 
 screen.exitonclick()
