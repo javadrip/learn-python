26745c29b8a3a80b9a1e3b495576f89798c738f4
Vinleon Ang <javadripdev@gmail.com>
2023-08-25 12:46:17 +0900

    Enable snake to turn smoothly

--- angela-yu-100-days-of-code/snake-game/main.py
+++ angela-yu-100-days-of-code/snake-game/main.py
@@ -40,7 +40,12 @@ while game_is_on:
     # i.e. snake appears to move as one whole instead of segment by segment
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+    segments[0].forward(20)
 
 screen.exitonclick()
