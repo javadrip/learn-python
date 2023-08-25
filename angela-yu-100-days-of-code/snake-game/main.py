cd51a49a8d832b60caacd93e3961439b1055c3e4
Vinleon Ang <javadripdev@gmail.com>
2023-08-25 13:05:48 +0900

    Get snake to appear on screen using class Snake

--- angela-yu-100-days-of-code/snake-game/main.py
+++ angela-yu-100-days-of-code/snake-game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -22,30 +23,40 @@ screen.tracer(0)
 # segment_3.goto(-40, 0)
 
 # Using a for loop to create segments
-starting_positions = [(0,0), (-20, 0), (-40,0)]
+# starting_positions = [(0,0), (-20, 0), (-40,0)]
 
-segments = []
+# segments = []
 
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+# for position in starting_positions:
+#     new_segment = Turtle("square")
+#     new_segment.color("white")
+#     new_segment.penup()
+#     new_segment.goto(position)
+#     segments.append(new_segment)
+
+# game_is_on = True
+
+# while game_is_on:
+#     # Updates the screen only after each segment is looped through
+#     # i.e. snake appears to move as one whole instead of segment by segment
+#     screen.update()
+#     time.sleep(0.1)
+
+#     for seg_num in range(len(segments) - 1, 0, -1):
+#         new_x = segments[seg_num - 1].xcor()
+#         new_y = segments[seg_num - 1].ycor()
+#         segments[seg_num].goto(new_x, new_y)
+
+#     segments[0].forward(20)
+
+# screen.exitonclick()
+
+# Using Snake class
+snake = Snake()
 
 game_is_on = True
-
 while game_is_on:
-    # Updates the screen only after each segment is looped through
-    # i.e. snake appears to move as one whole instead of segment by segment
     screen.update()
     time.sleep(0.1)
-
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-
-    segments[0].forward(20)
 
 screen.exitonclick()
