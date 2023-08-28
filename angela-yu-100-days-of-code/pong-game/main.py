806a2c45cf4fff931fa3576eaa7cf63bdaba7635
Vinleon Ang <javadripdev@gmail.com>
2023-08-28 21:00:05 +0800

    Create left and right paddles

--- angela-yu-100-days-of-code/pong-game/main.py
+++ angela-yu-100-days-of-code/pong-game/main.py
@@ -1,8 +1,18 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
