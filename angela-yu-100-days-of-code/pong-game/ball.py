ff4bf1dc7003034c5de0327799b15a07d4d16c7a
Vinleon Ang <javadripdev@gmail.com>
2023-08-28 22:12:20 +0800

    Refactor code and clean up

--- angela-yu-100-days-of-code/pong-game/ball.py
+++ angela-yu-100-days-of-code/pong-game/ball.py
@@ -1,14 +1,18 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 10
+MOVE_SPEED = 0.1
+MOVE_SPEED_FACTOR = 0.9
+
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.color("white")
         self.penup()
-        self.x_move = 10
-        self.y_move = 10
-        self.move_speed = 0.1
+        self.x_move = MOVE_DISTANCE
+        self.y_move = MOVE_DISTANCE
+        self.move_speed = MOVE_SPEED
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -20,9 +24,9 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
-        self.move_speed *= 0.9
+        self.move_speed *= MOVE_SPEED_FACTOR
 
     def reset_position(self):
         self.goto(0, 0)
-        self.move_speed = 0.1
+        self.move_speed = MOVE_SPEED
         self.bounce_x()
