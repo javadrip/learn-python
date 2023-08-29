22ae3df3ef830fc13bcdb55f02e3d375b72627b0
Vinleon Ang <javadripdev@gmail.com>
2023-08-29 10:46:44 +0800

    Regulate car propagation

--- angela-yu-100-days-of-code/turtle-crossing/car_manager.py
+++ angela-yu-100-days-of-code/turtle-crossing/car_manager.py
@@ -11,12 +11,16 @@ class CarManager():
         self.all_cars = []
 
     def create_car(self):
-        new_car = Turtle("square")
-        new_car.shapesize(stretch_wid=1, stretch_len=2)
-        new_car.color(random.choice(COLORS))
-        new_car.penup()
-        new_car.goto(300, random.randint(-250, 250))
-        self.all_cars.append(new_car)
+        # random_chance slows down the creation of the car.
+        # Without it, 1 car is generated every 0.1 seconds (as defined in main.py).
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            new_car.penup()
+            new_car.goto(300, random.randint(-250, 250))
+            self.all_cars.append(new_car)
 
     def move_cars(self):
         for car in self.all_cars:
