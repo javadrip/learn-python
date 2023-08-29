d7e251a982809f2be5444164ef8b14950e8e25e4
Vinleon Ang <javadripdev@gmail.com>
2023-08-29 10:42:53 +0800

    Add move_cars method

--- angela-yu-100-days-of-code/turtle-crossing/car_manager.py
+++ angela-yu-100-days-of-code/turtle-crossing/car_manager.py
@@ -17,3 +17,7 @@ class CarManager():
         new_car.penup()
         new_car.goto(300, random.randint(-250, 250))
         self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
