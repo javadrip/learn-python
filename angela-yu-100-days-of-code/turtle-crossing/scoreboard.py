62ce13f4978934bad07666ec5aa23758af1a70c0
javadrip <javadripdev@gmail.com>
2023-08-29 11:22:54 +0800

    Define Scoreboard class

--- angela-yu-100-days-of-code/turtle-crossing/scoreboard.py
+++ angela-yu-100-days-of-code/turtle-crossing/scoreboard.py
@@ -1,5 +1,14 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
+ALIGNMENT = "left"
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.goto(-290, 270)
+        self.write(f"Level: {self.level}", align=ALIGNMENT, font=FONT)
+        self.hideturtle()
