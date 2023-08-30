75830b6b64bbdf75f320b2366dfea9db0075382f
javadrip <javadripdev@gmail.com>
2023-08-30 18:27:15 +0800

    Use pandas to extract temp data

--- angela-yu-100-days-of-code/weather-record/main.py
+++ angela-yu-100-days-of-code/weather-record/main.py
@@ -3,15 +3,22 @@
 # Can be done as above but the output data will require a lot of cleaning.
 
 # We can use the inbuilt csv package
-import csv
+# import csv
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
 
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperature = int(row[1])
-            temperatures.append(temperature)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature = int(row[1])
+#             temperatures.append(temperature)
 
-    print(temperatures)
+#     print(temperatures)
+
+
+# Using pandas
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
