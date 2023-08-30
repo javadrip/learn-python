f36d19bf93a47337141ab6f2d923db011483ad1d
javadrip <javadripdev@gmail.com>
2023-08-30 17:20:14 +0800

    Enable storage of high score permanently

--- angela-yu-100-days-of-code/snake-game/scoreboard.py
+++ angela-yu-100-days-of-code/snake-game/scoreboard.py
@@ -8,7 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        # If using VSCode, make sure that checkbox in Settings > Python > Terminal: Execute in file dir is checked
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -31,6 +33,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
