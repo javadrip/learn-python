e00b5c2234224b6d64234fe3b43a70256c437f34
javadrip <javadripdev@gmail.com>
2023-08-30 18:02:35 +0800

    Extract data using csv

---
+++ angela-yu-100-days-of-code/weather-record/main.py
@@ -0,0 +1,17 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+# Can be done as above but the output data will require a lot of cleaning.
+
+# We can use the inbuilt csv package
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+
+    temperatures = []
+    for row in data:
+        if row[1] != "temp":
+            temperature = int(row[1])
+            temperatures.append(temperature)
+
+    print(temperatures)
