3d024e755a879622dfe0c4c40fc8603d5da7a3c2
javadrip <javadripdev@gmail.com>
2023-08-31 16:35:53 +0800

    US states game complete

--- angela-yu-100-days-of-code/us-states-game/main.py
+++ angela-yu-100-days-of-code/us-states-game/main.py
@@ -1,5 +1,7 @@
 import turtle
 
+import pandas
+
 IMAGE = "blank_states_img.gif"
 
 screen = turtle.Screen()
@@ -15,6 +17,33 @@ turtle.shape(IMAGE)
 # turtle.onscreenclick(get_mouse_click_coord)
 # turtle.mainloop()
 
-answer_state = screen.textinput(
-    title="Guess the state", prompt="What's another state's name?"
-)
+data = pandas.read_csv("50_states.csv")
+# Need to covert to list to use if loop.
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/50 states correct",
+        prompt="What's another state's name? (Type 'Exit' to exit the game)",
+    ).title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
+
+print(all_states)
