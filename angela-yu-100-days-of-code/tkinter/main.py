9fe5495cf9dd2dd276b0931d24db6ff04992baf6
Vinleon Ang <javadripdev@gmail.com>
2023-09-04 04:23:44 +0800

    Add button_clicked

--- angela-yu-100-days-of-code/tkinter/main.py
+++ angela-yu-100-days-of-code/tkinter/main.py
@@ -7,7 +7,10 @@ window.minsize(width=500, height=300)
 my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
 
-button = Button(text="Click me")
+def button_clicked():
+    print("I got clicked")
+
+button = Button(text="Click me", command=button_clicked)
 button.pack()
 
 window.mainloop()
