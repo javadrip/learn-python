98dab653b8f6675f524a452e366049e59e859472
Vinleon Ang <javadripdev@gmail.com>
2023-09-05 08:14:56 +0900

    Add text change after button click

--- angela-yu-100-days-of-code/tkinter/main.py
+++ angela-yu-100-days-of-code/tkinter/main.py
@@ -7,8 +7,13 @@ window.minsize(width=500, height=300)
 my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
 
+my_label["text"] = "New text"
+# my_label.config(text="New text")
+
 def button_clicked():
-    print("I got clicked")
+    my_label.config(text="Button got clicked")
+    # Line below works too
+    # my_label["text"] = "Button got clicked"
 
 button = Button(text="Click me", command=button_clicked)
 button.pack()
