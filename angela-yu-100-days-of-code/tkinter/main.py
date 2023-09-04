2249d49146e51aeef9c1c0f38fc8de1e60d836b2
Vinleon Ang <javadripdev@gmail.com>
2023-09-05 08:46:27 +0900

    Add placeholder text

--- angela-yu-100-days-of-code/tkinter/main.py
+++ angela-yu-100-days-of-code/tkinter/main.py
@@ -14,7 +14,8 @@ my_label["text"] = "New text"
 
 # Button click behaviour
 def button_clicked():
-    my_label.config(text="Button got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
     # Line below works too
     # my_label["text"] = "Button got clicked"
 
@@ -23,9 +24,9 @@ button = Button(text="Click me", command=button_clicked)
 button.pack()
 
 # Display input field
-input = Entry(width=10)
+input = Entry()
+input.insert(END, string="Some text to begin with.")
 input.pack()
-
 
 # Keep window open
 window.mainloop()
