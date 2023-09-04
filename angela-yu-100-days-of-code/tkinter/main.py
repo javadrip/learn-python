2365445809e5bcc39aeec5a83814e78c5e81ff19
Vinleon Ang <javadripdev@gmail.com>
2023-09-05 08:47:03 +0900

    Add text box

--- angela-yu-100-days-of-code/tkinter/main.py
+++ angela-yu-100-days-of-code/tkinter/main.py
@@ -28,5 +28,15 @@ input = Entry()
 input.insert(END, string="Some text to begin with.")
 input.pack()
 
+#Text
+text = Text(height=5, width=30)
+#Puts cursor in textbox.
+text.focus()
+#Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+#Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
+
 # Keep window open
 window.mainloop()
