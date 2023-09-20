f23c6e880c6a6847398fc247764edaeb67f63cbd
javadrip <javadripdev@gmail.com>
2023-09-20 17:22:51 +0800

    Remove border around canvas

--- angela-yu-100-days-of-code/pomodoro/main.py
+++ angela-yu-100-days-of-code/pomodoro/main.py
@@ -21,7 +21,7 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.pack()
