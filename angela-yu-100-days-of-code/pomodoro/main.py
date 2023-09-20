83ebe20ef6c4a7b59bb0899cdf05f8186869f32f
javadrip <javadripdev@gmail.com>
2023-09-20 17:44:01 +0800

    Add title_label

--- angela-yu-100-days-of-code/pomodoro/main.py
+++ angela-yu-100-days-of-code/pomodoro/main.py
@@ -16,15 +16,19 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", font=(FONT_NAME, 48, "bold"), fg=GREEN, bg=YELLOW)
+title_label.grid(row=0, column=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
 
 window.mainloop()
