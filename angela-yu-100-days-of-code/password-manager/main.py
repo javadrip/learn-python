b04afa9d78a5097010946b97efd9c04eb97eef86
javadrip <javadripdev@gmail.com>
2023-09-20 20:53:29 +0800

    Display logo

--- angela-yu-100-days-of-code/password-manager/main.py
+++ angela-yu-100-days-of-code/password-manager/main.py
@@ -1,5 +1,17 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width="200", height="200")
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_image)
+canvas.pack()
+
+window.mainloop()
