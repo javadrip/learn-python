4a47d775532f100c002f055db24f56f0cc3e692c
javadrip <javadripdev@gmail.com>
2023-09-20 21:38:05 +0800

    Create user interface

--- angela-yu-100-days-of-code/password-manager/main.py
+++ angela-yu-100-days-of-code/password-manager/main.py
@@ -12,6 +12,26 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width="200", height="200")
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+website_entry = Entry(width=41)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+username_label = Label(text="Email/Username:")
+username_label.grid(row=2, column=0)
+username_entry = Entry(width=41)
+username_entry.grid(row=2, column=1, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+password_entry = Entry(width=23)
+password_entry.grid(row=3, column=1)
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=38)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
