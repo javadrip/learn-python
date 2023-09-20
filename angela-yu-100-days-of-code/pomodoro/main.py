fb7797083cc89015c0930630350dc79bc36f4c62
javadrip <javadripdev@gmail.com>
2023-09-20 18:22:11 +0800

    Resolve single 0 on the minute

--- angela-yu-100-days-of-code/pomodoro/main.py
+++ angela-yu-100-days-of-code/pomodoro/main.py
@@ -23,6 +23,9 @@ def start_timer():
 def countdown(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec == 0:
+        count_sec = "00"
+
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, countdown, count - 1)
