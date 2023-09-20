7a0fca342a7cdf1606981ecd6203c28c933bdb52
javadrip <javadripdev@gmail.com>
2023-09-20 18:36:06 +0800

    Add reps and intervals

--- angela-yu-100-days-of-code/pomodoro/main.py
+++ angela-yu-100-days-of-code/pomodoro/main.py
@@ -10,13 +10,26 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    countdown(300)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        countdown(long_break_sec)
+    elif reps % 2 == 0:
+        countdown(short_break_sec)
+    else:
+        countdown(work_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
