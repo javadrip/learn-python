a870d1ccb158e34f32684cffdc53c4ff055f6508
javadrip <javadripdev@gmail.com>
2023-09-20 18:16:40 +0800

    Add coundown to timer_text

--- angela-yu-100-days-of-code/pomodoro/main.py
+++ angela-yu-100-days-of-code/pomodoro/main.py
@@ -17,7 +17,7 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def countdown(count):
-    print(count)
+    canvas.itemconfig(timer_text, text=count)
     if count > 0:
         window.after(1000, countdown, count - 1)
 
@@ -27,10 +27,19 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-countdown(5)
 
 title_label = Label(text="Timer", font=(FONT_NAME, 48, "bold"), fg=GREEN, bg=YELLOW)
 title_label.grid(row=0, column=1)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
+)
+canvas.grid(row=1, column=1)
+
+countdown(5)
 
 start_button = Button(text="Start", highlightbackground=YELLOW)
 start_button.grid(row=2, column=0)
@@ -40,12 +49,6 @@ reset_button.grid(row=2, column=2)
 
 check = Label(text="✔", font=(FONT_NAME, 48, "bold"), fg=GREEN, bg=YELLOW)
 check.grid(row=3, column=1)
-
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(row=1, column=1)
 
 
 window.mainloop()
