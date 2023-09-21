3172d377cba27cc4e2573ab5e9b6722886711285
javadrip <javadripdev@gmail.com>
2023-09-21 16:20:09 +0800

    Add error handling

--- angela-yu-100-days-of-code/nato-alphabet/main.py
+++ angela-yu-100-days-of-code/nato-alphabet/main.py
@@ -1,17 +1,26 @@
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+# 1. Create a dictionary in this format:
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-dictionary = {row.letter:row.code for (index, row) in data.iterrows()}
+dictionary = {row.letter: row.code for (index, row) in data.iterrows()}
 print(dictionary)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
 
-phonetic_code_words = [ dictionary[letter] for letter in word]
+def generate_phonetic():
+    # 2. Create a list of the phonetic code words from a word that the user inputs.
+    word = input("Enter a word: ").upper()
 
-print(phonetic_code_words)
+    try:
+        phonetic_code_words = [dictionary[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet, please.")
+        generate_phonetic()
+    else:
+        print(phonetic_code_words)
+
+
+generate_phonetic()
