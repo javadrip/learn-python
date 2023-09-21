217e09e33cf01849d2e2978624ad7bc82d13ed58
javadrip <javadripdev@gmail.com>
2023-09-21 18:40:09 +0800

    Add next word to buttons

--- angela-yu-100-days-of-code/flash-card-project/main.py
+++ angela-yu-100-days-of-code/flash-card-project/main.py
@@ -1,6 +1,19 @@
+import random
 from tkinter import *
 
+import pandas
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
 
 window = Tk()
 window.title("Flashy")
@@ -9,8 +22,12 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
-canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"), fill="black")
-canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"), fill="black")
+card_title = canvas.create_text(
+    400, 150, text="Title", font=("Arial", 40, "italic"), fill="black"
+)
+card_word = canvas.create_text(
+    400, 263, text="word", font=("Arial", 60, "bold"), fill="black"
+)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -19,6 +36,7 @@ cross_button = Button(
     image=cross_image,
     highlightthickness=0,
     highlightbackground=BACKGROUND_COLOR,
+    command=next_card,
 )
 cross_button.grid(row=1, column=0)
 
@@ -27,6 +45,7 @@ check_button = Button(
     image=check_image,
     highlightthickness=0,
     highlightbackground=BACKGROUND_COLOR,
+    command=next_card,
 )
 check_button.grid(row=1, column=1)
 
