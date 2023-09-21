691e25df629c8b5661d38cbf6838975fb8e3d0d0
javadrip <javadripdev@gmail.com>
2023-09-21 18:27:50 +0800

    Create card

--- angela-yu-100-days-of-code/flash-card-project/main.py
+++ angela-yu-100-days-of-code/flash-card-project/main.py
@@ -6,4 +6,12 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+canvas = Canvas(width=800, height=526)
+card_front = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front)
+canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"), fill="black")
+canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"), fill="black")
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
 window.mainloop()
