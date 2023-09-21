93d2622adc6e1ba316c4831a6e5b54e463906a9b
javadrip <javadripdev@gmail.com>
2023-09-21 18:43:36 +0800

    Clean up start up words

--- angela-yu-100-days-of-code/flash-card-project/main.py
+++ angela-yu-100-days-of-code/flash-card-project/main.py
@@ -23,10 +23,10 @@ canvas = Canvas(width=800, height=526)
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
 card_title = canvas.create_text(
-    400, 150, text="Title", font=("Arial", 40, "italic"), fill="black"
+    400, 150, text="", font=("Arial", 40, "italic"), fill="black"
 )
 card_word = canvas.create_text(
-    400, 263, text="word", font=("Arial", 60, "bold"), fill="black"
+    400, 263, text="", font=("Arial", 60, "bold"), fill="black"
 )
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
@@ -48,5 +48,7 @@ check_button = Button(
     command=next_card,
 )
 check_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
