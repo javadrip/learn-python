6de2f124cd40390858d294bb031351daeb01bbec
javadrip <javadripdev@gmail.com>
2023-09-21 21:48:45 +0800

    Add flip card and track progress

--- angela-yu-100-days-of-code/flash-card-project/main.py
+++ angela-yu-100-days-of-code/flash-card-project/main.py
@@ -4,24 +4,55 @@ from tkinter import *
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
+    global current_card, flip_timer
+    # To prevent concurrent timers if wrong or right buttons are pressed several times before the card flips
+    window.after_cancel(flip_timer)
+
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front)
+
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    # Index=False to not save the index column
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front)
+card_back = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front)
 card_title = canvas.create_text(
     400, 150, text="", font=("Arial", 40, "italic"), fill="black"
 )
@@ -45,7 +76,7 @@ check_button = Button(
     image=check_image,
     highlightthickness=0,
     highlightbackground=BACKGROUND_COLOR,
-    command=next_card,
+    command=is_known,
 )
 check_button.grid(row=1, column=1)
 
