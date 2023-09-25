3f410ee01ba3ae27db90a940daf90840ebcd7695
Vinleon Ang <javadripdev@gmail.com>
2023-09-25 22:44:54 +0800

    Complete step 3 Pull in IATA code

--- angela-yu-100-days-of-code/flight-deals/flight_search.py
+++ angela-yu-100-days-of-code/flight-deals/flight_search.py
@@ -1,13 +1,24 @@
+import os
 import requests
 
+from dotenv import load_dotenv
+load_dotenv()
+
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY = "YOUR_API_KEY_HERE"
+TEQUILA_API_KEY = os.getenv("TEQUILA_API_KEY")
 
 
 #This class is responsible for talking to the Flight Search API.
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code = "TESTING"
+        # 5. Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        # code = "TESTING"
+
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
