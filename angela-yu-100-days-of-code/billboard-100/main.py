1864917eda96a28a3bf0b1d7e01b89ec824a0386
Vinleon Ang <javadripdev@gmail.com>
2023-09-26 22:30:32 +0800

    Complete billboard 100 playlist

--- angela-yu-100-days-of-code/billboard-100/main.py
+++ angela-yu-100-days-of-code/billboard-100/main.py
@@ -1,9 +1,20 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+load_dotenv()
 
-# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
-URL = "https://www.billboard.com/charts/hot-100/2000-08-12"
+URL = "https://www.billboard.com/charts/hot-100/" + date
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_USERNAME = os.getenv("SPOTIFY_USERNAME")
+
+song_uris = []
+year = date.split("-")[0]
 
 response = requests.get(URL)
 billboard_web_page = response.text
@@ -12,4 +23,29 @@ soup = BeautifulSoup(billboard_web_page, "html.parser")
 song_names_spans = soup.select("li ul li h3")
 song_names = [song.getText().strip() for song in song_names_spans]
 
-print(song_names)
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="https://www.example.com/",
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt",
+        username=SPOTIFY_USERNAME,
+        )
+    )
+user_id = sp.current_user()["id"]
+
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    # print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+# print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
