54c60826964425be6bcd7a88311d953603bdab73
javadrip <javadripdev@gmail.com>
2023-09-29 14:28:39 +0800

    Click on content portals link

--- angela-yu-100-days-of-code/selenium/interaction.py
+++ angela-yu-100-days-of-code/selenium/interaction.py
@@ -14,3 +14,9 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
 print(article_count.text)
+
+# Find "Content portals" link by text and click it
+all_portals = driver.find_element(By.LINK_TEXT, "Content portals")
+all_portals.click()
+
+# driver.quit()
