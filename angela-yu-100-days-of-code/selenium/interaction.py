14e77c5f887ea2f4f8a85ea62c5a68ff0fc956f2
javadrip <javadripdev@gmail.com>
2023-09-29 15:17:04 +0800

    FIll in search and press enter

--- angela-yu-100-days-of-code/selenium/interaction.py
+++ angela-yu-100-days-of-code/selenium/interaction.py
@@ -1,6 +1,9 @@
+import time
+
 from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 ua = UserAgent()
 user_agent = ua.random
@@ -17,6 +20,10 @@ print(article_count.text)
 
 # Find "Content portals" link by text and click it
 all_portals = driver.find_element(By.LINK_TEXT, "Content portals")
-all_portals.click()
+# all_portals.click()
+
+# Fill in search box and hit ENTER
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python", Keys.RETURN)
 
 # driver.quit()
